18a6107e54d32424ac822cb9ee8238dc5539fb50
Collin Argo <collinargo@gmail.com>
2018-10-03 14:41:41 -0400

    all project-titles stick under nav bar

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,10 +1,13 @@
 var myNavBar = document.querySelector('.nav-bar');
 
-var projectTitle = document.querySelector('.project-title');
+var projectTitles = document.querySelectorAll('.project-title');
 
 // myNavBar.top = 0
 myNavBar.style.top = '0';
 
 // projectTitle.top = myNavBar.scrollHeight;
 var height = (String(myNavBar.scrollHeight) + 'px');
-projectTitle.style.top = height;
+projectTitlesLength = projectTitles.length
+for (var i = 0; i < projectTitlesLength; i++) {
+    projectTitles[i].style.top = height;
+}
