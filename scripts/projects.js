2b9aca6c8988f81f4b7ae8137d8752900e459473
Collin Argo <collinargo@gmail.com>
2018-10-03 15:33:18 -0400

    add class to project4 div

--- scripts/projects.js
+++ scripts/projects.js
@@ -2,12 +2,19 @@ var myNavBar = document.querySelector('.nav-bar');
 
 var projectTitles = document.querySelectorAll('.project-title');
 
-// myNavBar.top = 0
+// var projectLinks = document.querySelectorAll('.content a')
+
 myNavBar.style.top = '0';
 
-// projectTitle.top = myNavBar.scrollHeight;
-var height = (String(myNavBar.scrollHeight) + 'px');
+var height = (String(myNavBar.scrollHeight - 1) + 'px');
 projectTitlesLength = projectTitles.length
 for (var i = 0; i < projectTitlesLength; i++) {
     projectTitles[i].style.top = height;
 }
+
+// height = (String(myNavBar.scrollHeight + projectTitles[0].scrollHeight) + 'px');
+// projectLinksLength = projectLinks.length
+// for (var i = 0; i < projectLinksLength; i++) {
+//     projectLinks[i].style.paddingTop = height;
+//     projectLinks[i].style.marginTop = ('-' + height);
+// }
