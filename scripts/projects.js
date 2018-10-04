9b76ac578f2d13709e8e450043b5e89a992f5cbd
Collin Argo <collinargo@gmail.com>
2018-10-04 12:05:02 -0400

    set each project title height to max

--- scripts/projects.js
+++ scripts/projects.js
@@ -7,10 +7,18 @@ var projectTitles = document.querySelectorAll('.project-title');
 myNavBar.style.top = '0';
 
 var height = (String(myNavBar.scrollHeight - 1) + 'px');
-projectTitlesLength = projectTitles.length
+var max_title_height = projectTitles[0].scrollHeight;
+projectTitlesLength = projectTitles.length;
 for (var i = 0; i < projectTitlesLength; i++) {
     projectTitles[i].style.top = height;
+    if (projectTitles[i].style.height > max_title_height) {
+        max_title_height = projectTitles[i].scrollHeight;
+    }
 }
+for (var i = 0; i < projectTitlesLength; i++) {
+    projectTitles[i].style.height = String(max_title_height + 'px');
+}
+
 
 // height = (String(myNavBar.scrollHeight + projectTitles[0].scrollHeight) + 'px');
 // projectLinksLength = projectLinks.length
