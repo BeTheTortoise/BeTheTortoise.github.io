42d094b975b4ec04de95474847b980b773eb1346
Collin Argo <collinargo@gmail.com>
2018-10-04 12:38:59 -0400

    give project descriptions padding

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,28 +1,9 @@
 var myNavBar = document.querySelector('.nav-bar');
 
-var projectTitles = document.querySelectorAll('.project-title');
+var projectLinks = document.querySelectorAll('.nav-bar a');
 
-// var projectLinks = document.querySelectorAll('.content a')
-
-myNavBar.style.top = '0';
-
-var height = (String(myNavBar.scrollHeight - 1) + 'px');
-var max_title_height = projectTitles[0].scrollHeight;
-projectTitlesLength = projectTitles.length;
-for (var i = 0; i < projectTitlesLength; i++) {
-    projectTitles[i].style.top = height;
-    if (projectTitles[i].style.height > max_title_height) {
-        max_title_height = projectTitles[i].scrollHeight;
+for (i = 0; i < projectLinks.length; i++) {
+    projectLinks[i].onclick = function() {
+        window.scrollBy(0, myNavBar.scrollHeight);
     }
 }
-for (var i = 0; i < projectTitlesLength; i++) {
-    projectTitles[i].style.height = String(max_title_height + 'px');
-}
-
-
-// height = (String(myNavBar.scrollHeight + projectTitles[0].scrollHeight) + 'px');
-// projectLinksLength = projectLinks.length
-// for (var i = 0; i < projectLinksLength; i++) {
-//     projectLinks[i].style.paddingTop = height;
-//     projectLinks[i].style.marginTop = ('-' + height);
-// }
