1228b59cd7496a7787ccf28c17bb50531ab9b28e
Collin Argo <collinargo@gmail.com>
2018-10-07 13:01:51 -0400

    stick nav bar to bottom with js

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,7 @@
 var myNavBar = document.querySelector('.nav-bar');
 
+myNavBar.style.top = String((innerHeight - myNavBar.scrollHeight) + 'px')
+
 var projectLinks = document.querySelectorAll('.nav-bar a');
 
 for (i = 0; i < projectLinks.length; i++) {
