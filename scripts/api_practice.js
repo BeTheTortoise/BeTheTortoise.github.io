4cd242f86365aba7c6d0f63956d7c1c4becb1c95
Collin Argo <collinargo@gmail.com>
2018-10-17 13:25:59 -0400

    remove box shadow from nav-bar hover

--- scripts/api_practice.js
+++ scripts/api_practice.js
@@ -1,8 +1,8 @@
 const thisBody = document.querySelector('body');
 
-const h3Array = document.querySelectorAll('h3');
-const gotCharTrigger = document.querySelector('[data-trigger]');
-gotCharTrigger.addEventListener('click', (
+const gotOutput = document.querySelector('[data-output="got"]');
+const gotTrigger = document.querySelector('[data-trigger="got"]');
+gotTrigger.addEventListener('click', (
     () => {
         const charNum = Math.floor(Math.random() * 2000);
         let charString = '';
@@ -12,15 +12,47 @@ gotCharTrigger.addEventListener('click', (
                 console.log(j);
                 charString = j.titles.join(', ');
                 charString = charString + ' ' + j.name;
-                charString = charString + ', ' + j.aliases.join(', ');
+                if (j.aliases[0] != '') {
+                    charString = charString + ', ' + j.aliases.join(', ');
+                }
                 // charString = charString + 'hellooooo';
-                h3Array[1].textContent = charString;
+                gotOutput.textContent = charString;
             });
             
         //     .then( j => console.log(j) );
         // console.log(charObj);
     }
 ));
+
+const weatherOutput = document.querySelector('[data-output="weather"]');
+const weatherTrigger = document.querySelector('[data-trigger="weather"]').addEventListener('click', () => {
+    outputString = '';
+    fetch('https://api.openweathermap.org/data/2.5/weather?zip=30328,us&APPID=adfd069d135d0801d35399323f1ea34c')
+        .then( r => r.json())
+        .then( j => {
+            console.log(j);
+            weatherDescription = `${j[`weather`][0][`description`]}`;
+            highTemp = `${j[`main`][`temp_max`]}`;
+            lowTemp = `${j[`main`][`temp_min`]}`;
+            humidity = `${j[`main`][`humidity`]}`;
+            wind = `${j['wind']['speed']} mph at ${j['wind']['deg']} degrees`;
+            outputString = `In ${j['name']}, expect ${weatherDescription} with a low of ${lowTemp} and a high of ${highTemp}. Humidity is ${humidity}% and wind is ${wind}.`;
+            weatherOutput.textContent = outputString;
+        });
+});
+
+const twitterOutput = document.querySelector('[data-output="twitter"]');
+const twitterTrigger = document.querySelector('[data-trigger="twitter"]');
+twitterTrigger.addEventListener('click', () => {
+    const apiKey = 'V5dFFzruyW5YolYSCGjMlMLVn:kywj7HLYoxbspDbsuF0JUfBO0o9J0uvOmsNWYiEp0vUUqapfVc';
+    outputString = '';
+    fetch('https://api.openweathermap.org/data/2.5/weather?zip=30328,us&APPID=adfd069d135d0801d35399323f1ea34c')
+        .then( r => r.json())
+        .then( j => {
+            console.log(j);
+            twitterOutput.textContent = 'hello';
+        });
+});
 // fetch('https://api.icndb.com/jokes/random')
 // 	.then( r => r.json() )
 // 	.then( j => console.log(j) );
